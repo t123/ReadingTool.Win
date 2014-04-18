4e6f5b46ced79514335b23c9126591fdd38b958f
Travis Watt <travis.watt@gmail.com>
2014-04-18 11:08:11 +0100

    Cleaned up startup

--- RTWin/App.xaml.cs
+++ RTWin/App.xaml.cs
@@ -23,11 +23,8 @@ namespace RTWin
     /// </summary>
     public partial class App : Application
     {
-        private HubConnection _hubConnection;
-        private IHubProxy _mainHubProxy;
         public static User User { get; set; }
         private static IKernel _container;
-        private DatabaseService _databaseService;
 
         public static IKernel Container
         {
@@ -38,15 +35,7 @@ namespace RTWin
         {
             base.OnStartup(e);
 
-            InitContainer();
-
-            _databaseService = Container.Get<DatabaseService>();
-
-            BackupDb("Start");
-
-            InitDb();
-            InitWebApi();
-            InitSignalR();
+            _container = Setup.Start(_container);
 
             var skip = new Ninject.Parameters.ConstructorArgument("skip", true);
             var userDialog = Container.Get<UserDialog>(skip);
@@ -64,147 +53,9 @@ namespace RTWin
 
         protected override void OnExit(ExitEventArgs e)
         {
-            var db = App.Container.Get<Database>();
-            db.CloseSharedConnection();
-
-            BackupDb("Exit");
-
+            Setup.Shutdown(App.Container);
             WebCore.Shutdown();
             base.OnExit(e);
-        }
-
-        private void InitContainer()
-        {
-            _container = new StandardKernel();
-
-            _container.Bind<Database>().ToMethod(CreateDb);
-            _container.Bind<User>().ToMethod(x => App.User);
-
-            _container.Bind<DatabaseService>().ToSelf();
-            _container.Bind<UserService>().ToSelf();
-            _container.Bind<LanguageService>().ToSelf();
-            _container.Bind<LanguageCodeService>().ToSelf();
-            _container.Bind<PluginService>().ToSelf();
-
-            _container.Bind<UserDialog>().ToSelf();
-            _container.Bind<LanguageDialog>().ToSelf();
-            _container.Bind<ItemDialog>().ToSelf();
-            _container.Bind<PluginDialog>().ToSelf();
-        }
-
-        private static Database CreateDb(IContext context)
-        {
-            return new Database("db");
-        }
-
-        private void BackupDb(string identifier)
-        {
-            var backupDatabase = _databaseService.GetSetting<bool?>(DbSetting.Keys.BackupDatabase) ?? true;
-
-            if (!backupDatabase)
-            {
-                return;
-            }
-
-            var databaseFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Database", "rtwin.sqlite");
-
-            if (!File.Exists(databaseFile))
-            {
-                return;
-            }
-
-            var backupPath = _databaseService.GetSetting<string>(DbSetting.Keys.BackupDatabasePath);
-            var maxBackups = _databaseService.GetSetting<int?>(DbSetting.Keys.BackupMax) ?? 16;
-
-            if (string.IsNullOrWhiteSpace(backupPath))
-            {
-                backupPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Database", "Backup");
-            }
-
-            if (!Directory.Exists(backupPath))
-            {
-                Directory.CreateDirectory(backupPath);
-            }
-
-            string backupFile;
-            int counter = 1;
-            do
-            {
-                string filename = string.Format("{0:yyyyMMdd}-{1}-{2}.sqlite", DateTime.Now, identifier, counter);
-                backupFile = Path.Combine(backupPath, filename);
-                counter++;
-            } while (File.Exists(backupFile));
-
-            File.Copy(databaseFile, backupFile, false);
-
-            var directory = new DirectoryInfo(backupPath);
-
-            var difference = directory.GetFiles("*.sqlite").Length - maxBackups;
-            if (difference > 0)
-            {
-                var files = directory.GetFiles("*.sqlite").OrderBy(x => x.CreationTime).Take(difference);
-
-                foreach (var file in files)
-                {
-                    File.Delete(file.FullName);
-                }
-            }
-        }
-
-        private void InitDb()
-        {
-            _databaseService.CreateAndUpgradeDatabase();
-        }
-
-        private void InitWebApi()
-        {
-            WebApp.Start<OWINWebAPIConfig>(_databaseService.GetSetting<string>(DbSetting.Keys.BaseWebAPIAddress));
-        }
-
-        private void InitSignalR()
-        {
-            WebApp.Start<OWINSignalRConfig>(_databaseService.GetSetting<string>(DbSetting.Keys.BaseWebSignalRAddress));
-            _hubConnection = new HubConnection(_databaseService.GetSetting<string>(DbSetting.Keys.BaseWebSignalRAddress));
-            _mainHubProxy = _hubConnection.CreateHubProxy("MainHub");
-            InitHub();
-        }
-
-        private void InitHub()
-        {
-            _mainHubProxy.On<string, string>("addMessage", (element, action) =>
-            {
-                if (element == "video")
-                {
-                    System.Windows.Application.Current.Dispatcher.Invoke(() =>
-                    {
-                        double time;
-                        MainWindow owner = System.Windows.Application.Current.MainWindow as MainWindow;
-
-                        if (owner == null)
-                        {
-                            return;
-                        }
-
-                        if (!double.TryParse(action, out time) || owner.ReadControl == null)
-                        {
-                            _mainHubProxy.Invoke("Send", new object[] { "srtl1", -1 });
-                            _mainHubProxy.Invoke("Send", new object[] { "srtl2", -1 });
-                        }
-                        else
-                        {
-                            var sub = owner.ReadControl.GetSub(time);
-                            _mainHubProxy.Invoke("Send", new object[] { "srtl1", sub.Item1 });
-                            _mainHubProxy.Invoke("Send", new object[] { "srtl2", sub.Item2 });
-                        }
-                    });
-                }
-                else if (element == "modal")
-                {
-                    System.Windows.Application.Current.Dispatcher.InvokeAsync(() => Clipboard.SetText(action, TextDataFormat.UnicodeText), DispatcherPriority.Background);
-                }
-            });
-
-            _hubConnection.Start();
         }
     }
 }
---
+++ RTWin/Setup.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Threading;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.Owin.Hosting;
+using Ninject;
+using Ninject.Activation;
+using NPoco;
+using RTWin.Common;
+using RTWin.Controls;
+using RTWin.Entities;
+using RTWin.Services;
+
+namespace RTWin
+{
+    public class Setup
+    {
+        private IKernel _container;
+        private DatabaseService _databaseService;
+        private HubConnection _hubConnection;
+        private IHubProxy _mainHubProxy;
+
+        public IKernel Container { get { return _container; } }
+
+        public Setup(IKernel container)
+        {
+            _container = container;
+        }
+
+        public static IKernel Start(IKernel container)
+        {
+            var s = new Setup(container);
+            s.InitContainer();
+            s.BackupDb("Start");
+            s.InitDb();
+            s.InitWebApi();
+            s.InitSignalR();
+            return s.Container;
+        }
+
+        public static void Shutdown(IKernel container)
+        {
+            var s = new Setup(container);
+            var db = App.Container.Get<Database>();
+            db.CloseSharedConnection();
+            s.BackupDb("Exit");
+        }
+
+        private void InitContainer()
+        {
+            _container = new StandardKernel();
+
+            _container.Bind<Database>().ToMethod(CreateDb);
+            _container.Bind<User>().ToMethod(x => App.User);
+
+            _container.Bind<DatabaseService>().ToSelf();
+            _container.Bind<UserService>().ToSelf();
+            _container.Bind<LanguageService>().ToSelf();
+            _container.Bind<LanguageCodeService>().ToSelf();
+            _container.Bind<PluginService>().ToSelf();
+
+            _container.Bind<UserDialog>().ToSelf();
+            //_container.Bind<LanguageDialog>().ToSelf();
+            //_container.Bind<ItemDialog>().ToSelf();
+            //_container.Bind<PluginDialog>().ToSelf();
+
+            _databaseService = Container.Get<DatabaseService>();
+        }
+
+        private static Database CreateDb(IContext context)
+        {
+            return new Database("db");
+        }
+
+        public void BackupDb(string identifier)
+        {
+            if (_databaseService == null)
+            {
+                _databaseService = _container.Get<DatabaseService>();
+            }
+
+            var backupDatabase = _databaseService.GetSetting<bool?>(DbSetting.Keys.BackupDatabase) ?? true;
+
+            if (!backupDatabase)
+            {
+                return;
+            }
+
+            var databaseFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Database", "rtwin.sqlite");
+
+            if (!File.Exists(databaseFile))
+            {
+                return;
+            }
+
+            var backupPath = _databaseService.GetSetting<string>(DbSetting.Keys.BackupDatabasePath);
+            var maxBackups = _databaseService.GetSetting<int?>(DbSetting.Keys.BackupMax) ?? 16;
+
+            if (string.IsNullOrWhiteSpace(backupPath))
+            {
+                backupPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Database", "Backup");
+            }
+
+            if (!Directory.Exists(backupPath))
+            {
+                Directory.CreateDirectory(backupPath);
+            }
+
+            string backupFile;
+            int counter = 1;
+            do
+            {
+                string filename = string.Format("{0:yyyyMMdd}-{1}-{2}.sqlite", DateTime.Now, identifier, counter);
+                backupFile = Path.Combine(backupPath, filename);
+                counter++;
+            } while (File.Exists(backupFile));
+
+            File.Copy(databaseFile, backupFile, false);
+
+            var directory = new DirectoryInfo(backupPath);
+
+            var difference = directory.GetFiles("*.sqlite").Length - maxBackups;
+            if (difference > 0)
+            {
+                var files = directory.GetFiles("*.sqlite").OrderBy(x => x.CreationTime).Take(difference);
+
+                foreach (var file in files)
+                {
+                    File.Delete(file.FullName);
+                }
+            }
+        }
+
+        private void InitDb()
+        {
+            _databaseService.CreateAndUpgradeDatabase();
+        }
+
+        private void InitWebApi()
+        {
+            WebApp.Start<OWINWebAPIConfig>(_databaseService.GetSetting<string>(DbSetting.Keys.BaseWebAPIAddress));
+        }
+
+        private void InitSignalR()
+        {
+            WebApp.Start<OWINSignalRConfig>(_databaseService.GetSetting<string>(DbSetting.Keys.BaseWebSignalRAddress));
+            _hubConnection = new HubConnection(_databaseService.GetSetting<string>(DbSetting.Keys.BaseWebSignalRAddress));
+            _mainHubProxy = _hubConnection.CreateHubProxy("MainHub");
+
+            InitHub();
+        }
+
+        private void InitHub()
+        {
+            _mainHubProxy.On<string, string>("addMessage", (element, action) =>
+            {
+                if (element == "video")
+                {
+                    System.Windows.Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        double time;
+                        MainWindow owner = System.Windows.Application.Current.MainWindow as MainWindow;
+
+                        if (owner == null)
+                        {
+                            return;
+                        }
+
+                        //if (!double.TryParse(action, out time) || owner.ReadControl == null)
+                        //{
+                        //    _mainHubProxy.Invoke("Send", new object[] { "srtl1", -1 });
+                        //    _mainHubProxy.Invoke("Send", new object[] { "srtl2", -1 });
+                        //}
+                        //else
+                        //{
+                        //    var sub = owner.ReadControl.GetSub(time);
+                        //    _mainHubProxy.Invoke("Send", new object[] { "srtl1", sub.Item1 });
+                        //    _mainHubProxy.Invoke("Send", new object[] { "srtl2", sub.Item2 });
+                        //}
+                    });
+                }
+                else if (element == "modal")
+                {
+                    System.Windows.Application.Current.Dispatcher.InvokeAsync(() => Clipboard.SetText(action, TextDataFormat.UnicodeText), DispatcherPriority.Background);
+                }
+            });
+
+            _hubConnection.Start();
+        }
+    }
+}
